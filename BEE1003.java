9dd9930bab3168f1f49cd975c5c849c3ad8e9106
Prabal Chowdhury <prabal.k.chowdhury@gmail.com>
2021-11-22 00:06:50 +0600

    Create BEE1003.java

---
+++ BEE1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+  
+public class Main{
+  
+  public static void main(String[] args){
+    Scanner a = new Scanner(System.in);
+    
+    int  A=a.nextInt();
+    
+    
+    int  B=a.nextInt();
+    
+    int sum = A + B;
+    System.out.println("SOMA = "+ sum );
+    
+  }
+}
