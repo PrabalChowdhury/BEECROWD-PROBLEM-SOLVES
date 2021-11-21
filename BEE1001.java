86851114b5a64ed476e682c67d3869f97c040192
Prabal Chowdhury <prabal.k.chowdhury@gmail.com>
2021-11-22 00:05:09 +0600

    Create BEE1001.java

---
+++ BEE1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+  
+public class Main{
+  
+  public static void main(String[] args){
+    Scanner a = new Scanner(System.in);
+    
+    int  A=a.nextInt();
+  
+    
+    int  B=a.nextInt();
+    
+    int x = A + B;
+    System.out.println("X = "+ x );
+    
+  }
+}
