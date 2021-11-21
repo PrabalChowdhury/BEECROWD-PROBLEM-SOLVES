ede6b6fce0685fb57945ec4c83dcd3f2580b77d4
Prabal Chowdhury <prabal.k.chowdhury@gmail.com>
2021-11-22 00:08:51 +0600

    Create BEE1004.java

---
+++ BEE1004.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a*b;
+        System.out.println("PROD = "+c);       
+    }
+    
+}
